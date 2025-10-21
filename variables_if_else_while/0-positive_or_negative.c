0fb1cbf413565cc464f713305f8680c9298ff080
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-21 08:02:54 +0000

    test

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,33 @@
-file
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* Main - Fonction
+* Declaration varibale entier n
+* Fonction de calcul
+* Condition - if
+* Condition else if
+* Condition else
+* Main return - fonction
+* Return: retourne 0 si code valide
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("is positive\n");
+}
+else if (n == 0)
+{
+printf("is zero\n");
+}
+else
+{
+printf("is negative\n");
+}
+	return (0);
+}
