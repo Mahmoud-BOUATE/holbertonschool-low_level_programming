312ccf7bdffb0e827b40051433583ad9044d4009
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-21 13:53:32 +0000

    make task 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Fonction
+*
+* Declaration d'une chaine de caractère de type char
+* Affichage des aphabet en lowercase
+* Affichage de chaque lettre par ligne
+*
+* Return: retourne 0 si code valide
+*/
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i--)
+{
+putchar(i);
+}
+putchar('\n');
+
+return (0);
+}
+
