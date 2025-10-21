7b74613d180cb444cb209797df255b905cb9c9bc
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-21 13:24:37 +0000

    make task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - fonction
+* 
+* Déclaration de la variable i entier
+* Boucle for pour affichage des chiffres de 0-9
+*
+* Return: retourne 0 si code valide
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d", i);
+}
+return (0);
+}
