ccfa6a7fd641f15e7975ed0cd3b9df29a1143695
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-21 09:48:43 +0000

    test

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,20 +13,20 @@
 int main(void)
 {
 	int n;
-
+int last_digit;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 if (n > 5)
 {
-printf("Last digital of %d and is greather than 5\n", n);
+printf("Last digital of %d and is %d greather than 5\n", n, last_digit);
 }
 else if (n == 0)
 {
-printf("Last digital of %d and is 0\n", n);
+printf("Last digital of %d and is %d 0\n", n, last_digit);
 }
 else
 {
-printf("Last digital of %d and is less than 6 and not 0\n", n);
+printf("Last digital of %d and is less than 6 and not 0\n", n, last_digit);
 }
 	return (0);
 }
