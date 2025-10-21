3c1705a9261f12f7a21cbbe3bf3c437cdaea10b0
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-21 14:30:17 +0000

    make task 1

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - fonction
+*Affiche un message
+*Main return - fonction
+*Return: retourne 0 si code valide
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
