5f09c74c574cfc26648728a24f5663ccb3fdef74
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-21 14:11:09 +0000

    add commentaire

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+* main - Fonction
+*
+* Declaration d'une chaine de caractère de type char et int
+* Affichage des aphabet en lowercase de a-f et les chiffres de 0-9
+*
+* Return: retourne 0 si code valide
+*/
 int main(void)
 {
 int i;
