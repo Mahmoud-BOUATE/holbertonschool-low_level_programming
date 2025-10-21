bf439949699e7bc721b86f6d4d4c69d13fa87314
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-21 09:57:06 +0000

    test

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ last_digit = n % 10;
 printf("Last digit of %d is %d ", n, last_digit);
 if (last_digit > 5)
 {
-printf("and is greather than 5\n");
+printf("and is greater than 5\n");
 }
 else if (last_digit == 0)
 {
