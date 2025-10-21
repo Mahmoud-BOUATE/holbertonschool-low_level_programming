f3012f3ed0952c2dbf46d57e9ef459f7ae3a260d
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-21 12:49:34 +0000

    make task 4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,22 @@
-file
+#include <stdio.h>
+/**
+* main - Fonction
+*
+* Declaration d'une chaine de caractère de type char
+* Affichage des aphabet en lowercase and uppercase
+*
+* Return: retourne 0 si code valide
+*/
+int main(void)
+{
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+if (i != 'q' && i != 'e')
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
+}
