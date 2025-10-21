18cf5325d41e2780c80fde15e2caf840cf69f4da
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-21 08:06:53 +0000

    make 0-ask

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -23,11 +23,11 @@ printf("is positive\n");
 }
 else if (n < 0)
 {
-printf("is zero\n");
+printf("is negative\n");
 }
 else if (n == 0)
 {
-printf("is negative\n");
+printf("is zero\n");
 }
 	return (0);
 }
