a08a97cd8aca03d7a92ac55b7705ed9d8a291084
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-21 14:52:54 +0000

    test

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 /**
-*main - fonction
-*Affiche un message
-*Main return - fonction
-*Return: retourne 0 si code valide
-*/
+ * main - fonction principale
+ *
+ * Affiche _putchar suivi d’un retour à la ligne
+ *
+ * Return: retourne 0 si le code est valide
+ */
 int main(void)
 {
-printf("_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
