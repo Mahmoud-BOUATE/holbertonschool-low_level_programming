a77b783821de0321f4d241cf324e5ef1fb70007f
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-22 18:12:54 +0000

    add commenatire

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <unistd.h>
 /**
-* print_last_digit - fonction qui determine le signe des nombres
+* jack_bauer - fonction qui determine le signe des nombres
 * @n: la varible de retour
 * return: if true 1
 * Return: else false 0
@@ -18,7 +18,7 @@ for (hour = 0; hour < 24; hour++)
 {
 for (minute = 0; minute < 60; minute++)
 {
- _putchar((hour / 10) + '0');
+_putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
 _putchar(':');
 _putchar((minute / 10) + '0');
