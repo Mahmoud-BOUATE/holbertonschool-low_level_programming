799d4a142b74b9b870800b7856f6d30abeb798ee
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-22 12:40:54 +0000

    test

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <unistd.h>
+/**
+* print_alphabet_x10 - prints the lowercase alphabet
+* Boucle pour répéter les alphabets 10 fois
+*/
+int _islower(int c)
+{
+char r;
+if (c >= 'A' && c <= 'Z')
+{
+return (1)
+}
+else if
+{
+return (0)
+}
+_putchar('\n');
+}
+
+
+
+
+
