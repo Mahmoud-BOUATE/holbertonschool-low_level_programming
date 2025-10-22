b9149ce9e3d54053b49ad95e86baf957a12706f6
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-22 19:02:44 +0000

    correction

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,12 +1,15 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer,
- * starting from 00:00 to 23:59
+ * add - fonction pour addition
+ * @a: variable a pour addition
+ * @b: variable b
+ *
+ * Return: Always 0.
  */
 int add(int a, int b)
- {
+{
 int result;
 result = a + b;
 return (result);
- }
+}
