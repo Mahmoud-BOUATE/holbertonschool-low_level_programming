eecf33433f5f9f257f7edb6265bbc06835f73fa8
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-22 11:03:49 +0000

    make 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_alphabet - prints the lowercase alphabet
+*/
+void print_alphabet(void)
+{
+char i;
+int j;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+for (j = 0; j < 10; j++ )
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+
+
+
