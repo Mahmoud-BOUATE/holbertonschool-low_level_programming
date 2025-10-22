0e9b9cf7b0be95554c9f37a24f882850fa0ea61d
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-22 18:52:22 +0000

    test

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,10 +1,9 @@
 #include "main.h"
 #include <unistd.h>
 /**
-* jack_bauer - prints every minute of the day of Jack Bauer
-* @h: hour
-* @m: minute
-*/
+ * jack_bauer - prints every minute of the day of Jack Bauer,
+ * starting from 00:00 to 23:59
+ */
 
 void jack_bauer(void)
 {
