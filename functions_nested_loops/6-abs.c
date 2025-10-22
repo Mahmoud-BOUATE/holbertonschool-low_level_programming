903b539b773f298c802e3b706879b5c0f2b1c129
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-22 15:05:03 +0000

    answer task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <unistd.h>
+/**
+* print_sign - fonction qui determine le signe des nombres
+* @n: la varible de retour
+* return: if true 1
+* Return: else false 0
+*/
+int _abs(int n)
+{
+if (n < 0)
+return (-n);
+else
+return (n);
+}
+
+
+
+
+
+
+
