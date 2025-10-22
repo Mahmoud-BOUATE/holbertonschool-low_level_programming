488e918920278ad80402ab2e4397b51a42987731
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-22 15:05:54 +0000

    add commentaire

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <unistd.h>
 /**
-* print_sign - fonction qui determine le signe des nombres
+* _abs - fonction qui determine le signe des nombres
 * @n: la varible de retour
 * return: if true 1
 * Return: else false 0
