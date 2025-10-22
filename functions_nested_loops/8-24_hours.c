440f3a2aee33f21e25f666c304d86ec742d082f1
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-22 18:51:23 +0000

    test

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,16 +1,11 @@
 #include "main.h"
 #include <unistd.h>
 /**
-* jack_bauer - fonction qui determine le signe des nombres
-* @h: la varible pour compter les heres
-* @m: la varibal pour compter les minutes
-* return: if true 1
-* Return: else false 0
+* jack_bauer - prints every minute of the day of Jack Bauer
+* @h: hour
+* @m: minute
 */
-/**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
- */
+
 void jack_bauer(void)
 {
 int h, m;
