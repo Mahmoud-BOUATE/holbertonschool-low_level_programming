91ecfcc686f555607bde4117041ee34992f745db
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-22 08:30:18 +0000

    test

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -8,17 +8,5 @@ char i;
 for (i = 'a'; i <= 'z'; i++)
 _putchar(i);
 }
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-
-int main(void)
-{
-print_alphabet();
-_putchar('\n');
-return (0);
-}
 
 
