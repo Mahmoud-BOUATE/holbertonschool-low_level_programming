034cf51c2e098003eb3dc8495ed35dbef9172f1d
mahmoud BOUATE <12065@holbertonschool.com>
2025-10-25 17:16:44 +0000

    test

--- functions_nested_loops/11-main.c
+++ functions_nested_loops/11-main.c
@@ -1,27 +1,54 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_number - prints an integer using only _putchar
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+
+if (n / 10)
+print_number(n / 10);
+
+_putchar((n % 10) + '0');
+}
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: starting number
  */
 void print_to_98(int n)
 {
-
+if (n <= 98)
+{
 while (n <= 98)
 {
+print_number(n);
+if (n != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
 n++;
-printf("%d\n" , n);
 }
-if (n > 98)
+    }
+else
+    {
+while (n >= 98)
 {
+print_number(n);
+if (n != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
 n--;
-printf("%d\n" , n);
 }
-else if (n == 98)
-{
-printf("%d", n)
 }
 _putchar('\n');
 }
