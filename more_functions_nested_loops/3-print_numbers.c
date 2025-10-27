cbfbdc858ac8bba2d67f0a341afd84f02d9397dd
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-27 10:29:56 +0000

    test

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,15 @@
 #include <ctype.h>
+#include <unistd.h>
 /**
 * print_numbers - function that print numbers
 *
 *Return: Always 0.
 */
+void print_numbers(void)
+{
+int i;
+for (i =0; i <= 9; i++)
+{
+_putchar(i);
+}
+}
