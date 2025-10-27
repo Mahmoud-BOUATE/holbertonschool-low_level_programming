61fb4d7d528977763768c0be5b38baba0349293f
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-27 11:18:14 +0000

    test

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,9 @@ for (i = 0; i < 10; i++)
 int j;
 for (j = 0; j <= 14; j++)
 {
-if (j < 10)
-_putchar('0' + j);
-else
-{
-_putchar('0' + j / 10);
+if (j >= 10)
+_putchar('1');
 _putchar('0' + j % 10);
-}
 }
 _putchar('\n');
 }
