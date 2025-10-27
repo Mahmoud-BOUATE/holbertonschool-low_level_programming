d05ddf3c7115b0cf1a06677ca3791b26e08b935e
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-27 13:03:39 +0000

    correction

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_diagonal - prints a diagonal line of '\' characters
+ * @n: number of characters / lines
+ */
+void print_diagonal(int n)
+{
+int i, j;
+if (n <= 0)
+{
+_putchar('\n');  
+return;
+}
+
+for (i = 0; i < n; i++)
+{
+        
+for (j = 0; j < i; j++)
+_putchar(' ');
+
+_putchar('\\');  
+_putchar('\n');  
+}
+}
