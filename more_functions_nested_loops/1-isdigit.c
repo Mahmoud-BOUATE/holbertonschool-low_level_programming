f129aab580dd6c5cc336c2876ae3fb4e49e301c9
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-27 09:53:44 +0000

    test

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,20 @@
-file
+#include <ctype.h>
+/**
+* _isdigit - checks for a digit
+* @c: varibale de retour
+*
+*Return: Always 0.
+*/
+
+int _isdigit(int c)
+{
+if (c >= '1' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -2,4 +2,5 @@
 #define MAIN_H
 
 int _isupper(int c_i);
+int _isdigit(int c);
 #endif /* MAIN_H */
