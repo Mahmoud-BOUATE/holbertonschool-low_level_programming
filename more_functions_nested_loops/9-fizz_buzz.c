89a777a235c69efd56f8c91bf50afb2b54b40110
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-27 21:05:50 +0000

    test

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,20 +1,37 @@
 #include "main.h"
-#include <unistd.h>
+
 /**
- * print_square - prints a diagonal line of '\' characters
- * @size: number of characters / lines
+ * main - prints numbers from 1 to 100 with FizzBuzz rules
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-char *s = "Bizz\n";
-int i;
+int i, j;
+char *str;
 
-
-for (i = 0; s[i] != '\0'; i++)
+for (i = 1; i <= 100; i++)
 {
-_putchar(s[i]);
+if (i % 3 == 0 && i % 5 == 0)
+str = "FizzBuzz";
+else if (i % 3 == 0)
+str = "Fizz";
+else if (i % 5 == 0)
+str = "Buzz";
+else
+{
+if (i >= 10)
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+if (i != 100)
+_putchar(' ');
+continue;
 }
-
-
+for (j = 0; str[j] != '\0'; j++)
+_putchar(str[j]);
+if (i != 100)
+_putchar(' ');
+}
+_putchar('\n');
 return (0);
 }
