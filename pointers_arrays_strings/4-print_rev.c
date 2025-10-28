82f59f5577e7241b2c6913f22139d6b521da2060
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-28 15:03:37 +0000

    test

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _puts - afficher un phrase
- * @str: varibale pointeur
+ * print_rev - affiche une chaîne de caractères à l’envers
+ * @s: pointeur vers la chaîne de caractères à inverser
+ *
+ * Description: Cette fonction affiche les caractères d’une chaîne
+ * dans l’ordre inverse, suivis d’un retour à la ligne.
  */
 void print_rev(char *s)
 {
-int count = 0;
-while (s[count] != '\0')
-{
-count++;
-}
-while (count--)
-{
-_putchar(s[count]);
-}
-_putchar('\n');
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+
+	while (count--)
+	{
+		_putchar(s[count]);
+	}
+
+	_putchar('\n');
 }
