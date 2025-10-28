b1df5630e803e0fa8a4e5e1025bf5ab54d06d960
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-28 15:18:37 +0000

    test

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - afficher un phrase
+ * @str: varibale pointeur
+ */
+void rev_string(char *s);
+ {
+int len;
+while (s[len] != '\0')
+{
+len--;
+_putchar(s[len]);
+}
+_putchar('\n');
+ }
