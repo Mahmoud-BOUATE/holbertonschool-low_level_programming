7a1bdf0e8bf2d61500684dbb7dbf755f13b6a789
mahoud BOUATE <12065@holbertonstudent.com>
2025-10-29 13:33:19 +0000

    test

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _atoi - converts a string to an integer
  * @s: string to convert
@@ -7,22 +6,20 @@
  */
 int _atoi(char *s)
 {
-    int i = 0, sign = 1, num = 0;
-
-    if (s[i] == '-')
-    {
-        sign = -1;
-        i++;
-    }
-    else if (s[i] == '+')
-    {
-        i++;
-    }
-    while (s[i] >= '0' && s[i] <= '9')
-    {
-        num = num * 10 + (s[i] - '0');
-        i++;
-    }
-
-    return num * sign;
+int i = 0, sign = 1, num = 0;
+if (s[i] == '-')
+{
+sign = -1;
+i++;
+}
+else if (s[i] == '+')
+{
+i++;
+}
+while (s[i] >= '0' && s[i] <= '9')
+{
+num = num * 10 + (s[i] - '0');
+i++;
+}
+return (num * sign);
 }
