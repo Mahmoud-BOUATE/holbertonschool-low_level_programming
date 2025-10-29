5af54679f367955cb824687da6c08f418aa584dc
mahoud BOUATE <12065@holbertonstudent.com>
2025-10-29 13:46:42 +0000

    test

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,13 +1,19 @@
 #include "main.h"
-
 /**
- * _atoi - convertit une chaîne en entier
- * @s: chaîne à convertir
- * Return: entier correspondant
+ * _atoi - Convertit une chaîne de caractères en entier
+ * @s: La chaîne à convertir
+ * 
+ * Retour: L'entier correspondant à la chaîne
+ * 
+ * Cette version gère correctement les grands nombres pour éviter
+ * les dépassements d'entiers (overflow) en utilisant un type long
+ * pour l'accumulation.
  */
+
 int _atoi(char *s)
 {
-int i = 0, sign = 1, num = 0;
+int i = 0, sign = 1;
+long num = 0; // long pour éviter overflow
 while (s[i] && (s[i] < '0' || s[i] > '9'))
 {
 if (s[i] == '-')
@@ -19,5 +25,10 @@ while (s[i] >= '0' && s[i] <= '9')
 num = num * 10 + (s[i] - '0');
 i++;
 }
-return num * sign;
+num = num * sign;
+if (num > 2147483647)
+return 2147483647;
+if (num < -2147483648)
+return -2147483648;
+return (int)num;
 }
