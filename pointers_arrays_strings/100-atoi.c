576c8519deb016c4cd6a66143e17101c5ccf32d5
mahoud BOUATE <12065@holbertonstudent.com>
2025-10-29 13:52:27 +0000

    tet

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,30 +7,27 @@
  */
 int _atoi(char *s)
 {
-    int i = 0, sign = 1, result = 0;
-    int number_started = 0;
-
-    /* Ignore characters until the first digit, handle signs */
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-' || s[i] == '+')
-        {
-            if (number_started)
-                break;  /* stop at signs after number starts */
-            if (s[i] == '-')
-                sign *= -1;
-        }
-        else if (s[i] >= '0' && s[i] <= '9')
-        {
-            number_started = 1;
-            result = result * 10 + (s[i] - '0');
-        }
-        else if (number_started)
-        {
-            break;  /* stop at first non-digit after number starts */
-        }
-        i++;
-    }
-
-    return (result * sign);
+int i = 0, sign = 1, result = 0;
+int number_started = 0;
+while (s[i] != '\0')
+{
+if (s[i] == '-' || s[i] == '+')
+{
+if (number_started)
+break;
+if (s[i] == '-')
+sign *= -1;
+}
+else if (s[i] >= '0' && s[i] <= '9')
+{
+number_started = 1;
+result = result * 10 + (s[i] - '0');
+}
+else if (number_started)
+{
+break;
+}
+i++;
+}
+return (result *sign);
 }
