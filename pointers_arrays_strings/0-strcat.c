3451eef2f3a57983b99f3878c5dab7cf46d05fdd
mahoud BOUATE <12065@holbertonstudent.com>
2025-10-29 15:26:53 +0000

    test

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - function that returns the lenght of a string
+ * @s:  Pointeur
+ *
+ * Return: count.
+ */
+char *_strcat(char *dest, char *src)
+{
+if (src)
+return (src);
+else if (dest)
+return (dest);
+else
+{
+return (dest);
+return (src);
+_putchar('\n');
+}
+
+}
