671351aab48a2666ba6dd10aa959017e90017a5f
mahoud BOUATE <12065@holbertonstudent.com>
2025-10-29 13:53:17 +0000

    test

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -7,27 +7,32 @@
  */
 int _atoi(char *s)
 {
-int i = 0, sign = 1, result = 0;
-int number_started = 0;
-while (s[i] != '\0')
-{
-if (s[i] == '-' || s[i] == '+')
-{
-if (number_started)
-break;
-if (s[i] == '-')
-sign *= -1;
-}
-else if (s[i] >= '0' && s[i] <= '9')
-{
-number_started = 1;
-result = result * 10 + (s[i] - '0');
-}
-else if (number_started)
-{
-break;
-}
-i++;
-}
-return (result *sign);
+    int i = 0, sign = 1, result = 0;
+    int num_started = 0;
+
+    while (s[i] != '\0')
+    {
+        if (s[i] == '-' && !num_started)
+            sign *= -1;
+        else if (s[i] == '+' && !num_started)
+            ; /* ignore */
+        else if (s[i] >= '0' && s[i] <= '9')
+        {
+            num_started = 1;
+            if (result > (2147483647 - (s[i] - '0')) / 10)
+            {
+                /* simulate int overflow behavior like standard atoi */
+                if (sign == 1)
+                    return 2147483647;
+                else
+                    return -2147483648;
+            }
+            result = result * 10 + (s[i] - '0');
+        }
+        else if (num_started)
+            break; /* stop at first non-digit after number starts */
+        i++;
+    }
+
+    return result * sign;
 }
