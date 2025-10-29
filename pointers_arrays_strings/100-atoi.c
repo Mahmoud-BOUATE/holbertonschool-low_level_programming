5d22ce5692741196fd1848343207474c275ed159
mahoud BOUATE <12065@holbertonstudent.com>
2025-10-29 13:47:46 +0000

    test

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,34 +1,32 @@
 #include "main.h"
-/**
- * _atoi - Convertit une chaîne de caractères en entier
- * @s: La chaîne à convertir
- * 
- * Retour: L'entier correspondant à la chaîne
- * 
- * Cette version gère correctement les grands nombres pour éviter
- * les dépassements d'entiers (overflow) en utilisant un type long
- * pour l'accumulation.
- */
 
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to convert
+ * Return: integer representation of the string
+ */
 int _atoi(char *s)
 {
-int i = 0, sign = 1;
-long num = 0; // long pour éviter overflow
-while (s[i] && (s[i] < '0' || s[i] > '9'))
-{
-if (s[i] == '-')
-sign = -sign;
-i++;
-}
-while (s[i] >= '0' && s[i] <= '9')
-{
-num = num * 10 + (s[i] - '0');
-i++;
-}
-num = num * sign;
-if (num > 2147483647)
-return 2147483647;
-if (num < -2147483648)
-return -2147483648;
-return (int)num;
+    int i = 0, sign = 1, result = 0;
+
+    /* Ignore any characters before the first number or sign */
+    while (s[i] != '\0')
+    {
+        if (s[i] == '-')
+            sign *= -1;
+        else if (s[i] == '+')
+            sign *= 1;
+        else if (s[i] >= '0' && s[i] <= '9')
+            break;
+        i++;
+    }
+
+    /* Convert the numeric part */
+    while (s[i] >= '0' && s[i] <= '9')
+    {
+        result = result * 10 + (s[i] - '0');
+        i++;
+    }
+
+    return (result * sign);
 }
