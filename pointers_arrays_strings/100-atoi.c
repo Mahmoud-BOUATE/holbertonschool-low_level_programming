3c32688b7d4a9fe823157f97828a8a1b103de103
mahoud BOUATE <12065@holbertonstudent.com>
2025-10-29 13:36:55 +0000

    correction

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _atoi - convertit une chaîne en entier
  * @s: chaîne à convertir
@@ -7,13 +8,10 @@
 int _atoi(char *s)
 {
 int i = 0, sign = 1, num = 0;
+while (s[i] && (s[i] < '0' || s[i] > '9'))
+{
 if (s[i] == '-')
-{
-sign = -1;
-i++;
-}
-else if (s[i] == '+')
-{
+sign = -sign;
 i++;
 }
 while (s[i] >= '0' && s[i] <= '9')
@@ -21,6 +19,5 @@ while (s[i] >= '0' && s[i] <= '9')
 num = num * 10 + (s[i] - '0');
 i++;
 }
-
 return num * sign;
 }
