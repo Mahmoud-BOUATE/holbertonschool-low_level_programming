734da8dae99d0aa35c969114fb772fd107a07518
mahmoud BOUATE <12065@holbertonschool.com>
2025-10-30 09:41:48 +0000

    test

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <string.h>
 /**
- * _strlen - function that returns the lenght of a string
- * @s:  Pointeur
- *
- * Return: count.
+ * _strcat - function that returns the lenght of a string
+ * @dest: Tableau contenant la chaine de caractère 'Hello'
+ * @src: Tableau contenant la chaine de caractère 'World!'
+ * Return: Tableau contanant la chaine de caratère 'Hello world!'
  */
 char *_strcat(char *dest, char *src)
 {
@@ -13,7 +13,7 @@ while (dest[i] != '\0')
 {
 i++;
 }
-while(src[j] != '\0')
+while (src[j] != '\0')
 {
 dest[i] = src[j];
 j++;
