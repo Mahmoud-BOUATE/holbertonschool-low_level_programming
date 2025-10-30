9a3e7e875ce51aa1d94c457078fa1113d6305245
mahmoud BOUATE <12065@holbertonschool.com>
2025-10-30 15:59:59 +0000

    test

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _strcmp - this function compares two strings
- * @s1: chaine à comparer 
- * @s2 chaine à comparer
+ * @s1: chaine à comparer
+ * @s2: chaine à comparer
  *
  * Return: pointer to dest
  */
@@ -20,4 +20,4 @@ if (s1[i] == '\0' && s2[i] == '\0')
 return (0);
 else
 return (s1[i] - s2[i]);
-}.
+}
