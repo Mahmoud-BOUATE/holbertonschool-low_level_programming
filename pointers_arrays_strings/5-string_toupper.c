56fc3782b16cc59c7a7948842815fc3dcec30a79
mahmoud BOUATE <12065@holbertonschool.com>
2025-10-30 16:49:22 +0000

    push

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - inverse l'ordre des éléments d'un tableau d'entiers
+ * @a: le tableau à inverser
+ * @n: le nombre d'éléments dans le tableau
+ *
+ * Cette fonction échange les éléments du début et de la fin
+ * du tableau jusqu'à ce que tous les éléments soient inversés.
+ */
+char *string_toupper(char *)
+{
+char i = 0;
+while (i != '\0')
+{
+if ('a' < i < 'z')
+
+}
+}
