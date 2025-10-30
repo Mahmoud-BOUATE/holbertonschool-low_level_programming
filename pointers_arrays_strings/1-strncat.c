efd7010ca86a6be407e4846ecdfad8efad6abee9
mahmoud BOUATE <12065@holbertonschool.com>
2025-10-30 12:39:21 +0000

    etst

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - function that returns the lenght of a string
+ * @dest: Tableau contenant la chaine de caractère 'Hello'
+ * @src: Tableau contenant la chaine de caractère 'World!'
+ * Return: Tableau contanant la chaine de caratère 'Hello world!'
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, j = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+while (src[j] != '\0')
+{
+if (j < n)
+dest[i] = src[j];
+j++;
+i++;
+}
+return (dest);
+}
