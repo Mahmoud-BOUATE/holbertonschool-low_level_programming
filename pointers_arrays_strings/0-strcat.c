db1576971e8c46d488560d71aa46ab8e9e11837a
mahmoud BOUATE <12065@holbertonschool.com>
2025-10-30 09:33:03 +0000

    test

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -8,15 +8,16 @@
  */
 char *_strcat(char *dest, char *src)
 {
-if (src)
-return (src);
-else if (dest)
-return (dest);
-else
+int i = 0, j = 0;
+while (dest[i] != '\0')
 {
-return (dest);
-return (src);
-_putchar('\n');
+i++;
 }
-
+while(src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+return (dest);
 }
