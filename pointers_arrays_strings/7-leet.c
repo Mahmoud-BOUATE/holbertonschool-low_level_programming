7e2a37f004d902e31ab38f337c387baeccb4a8e1
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-31 11:02:33 +0000

    test

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <ctype.h>
+#include <stddef.h>
+/**
+ * leet - convertit quelques lettres en chiffres
+ * @s: la chaîne à convertir
+ *
+ * Retourne le même pointeur str (ou NULL si str est NULL).
+ *Return: str
+ */
+char *leet(char *s)
+{
+char tab1[] = "aAeEoOtTlL";
+char tab2[] = "7733007711";
+int i = 0; 
+int j;
+while (s[i] != '\0')
+{
+j = 0;
+while (tab1[j] != '\0' && tab2[j] != '\0')
+{
+if (s[i] == tab1[j])
+{
+s[i] = tab2[j];
+break;
+}
+j++;
+}
+i++;
+}
+return (s);
+}
