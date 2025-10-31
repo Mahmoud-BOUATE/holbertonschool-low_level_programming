b421f8775c13622056eee51a5b946312c2c70a13
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-31 09:09:08 +0000

    test

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,28 +1,23 @@
 #include "main.h"
-#include <ctype.h>   /* pour toupper (optionnel) */
+#include <ctype.h>
 #include <stddef.h>
-
 /**
  * string_toupper - convertit une chaîne en majuscules
  * @str: la chaîne à convertir
  *
  * Retourne le même pointeur str (ou NULL si str est NULL).
+ *Return: str
  */
 char *string_toupper(char *str)
 {
-    int i = 0;
-
-    if (str == NULL)
-        return NULL;
-
-    while (str[i] != '\0')
-    {
-    
+int i = 0;
+if (str == NULL)
+return (NULL);
+while (str[i] != '\0')
+{
 if (str[i] >= 'a' && str[i] <= 'z')
 str[i] = str[i] - 32;
 i++;
 }
-return str;
+return (str);
 }
-
-
