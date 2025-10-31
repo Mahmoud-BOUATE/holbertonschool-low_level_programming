c4a8a024a04d732fd6f8265ea209ab09ce464a75
mahmoud BOUATE <12065@holbertonstudents.com>
2025-10-31 13:34:58 +0000

    test

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,9 @@
 #include "main.h"
+
 /**
  * cap_string - met en majuscule la première lettre de chaque mot
  * @s: chaîne à modifier
+ *
  * Return: le même pointeur s
  */
 char *cap_string(char *s)
@@ -13,11 +15,18 @@ if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
 {
 s[i] = s[i] - 32;
 }
-else if ((s[i-1] == ' '  || s[i-1] == '\t' || s[i-1] == '\n' || s[i-1] == ','  || s[i-1] == ';'  || s[i-1] == '.'  || s[i-1] == '!'  || s[i-1] == '?'  || s[i-1] == '"'  || s[i-1] == '('  || s[i-1] == ')'  || s[i-1] == '{'  || s[i-1] == '}') && (s[i] >= 'a' && s[i] <= 'z'))
+else if ((s[i - 1] == ' ' || s[i - 1] == '\t' ||
+s[i - 1] == '\n' || s[i - 1] == ',' ||
+s[i - 1] == ';' || s[i - 1] == '.' ||
+s[i - 1] == '!' || s[i - 1] == '?' ||
+s[i - 1] == '"' || s[i - 1] == '(' ||
+s[i - 1] == ')' || s[i - 1] == '{' ||
+s[i - 1] == '}') &&
+(s[i] >= 'a' && s[i] <= 'z'))
 {
 s[i] = s[i] - 32;
 }
 i++;
 }
-return s;
+return (s);
 }
