d47466e43bc4b0e4d4629f85e3149998924499e0
mahmoud BOUATE <12065@holbertonstudents.com>
2025-11-02 15:17:07 +0000

    teste

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -1,34 +1,25 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * print_number - function that print entier
- * @n: variable
-*/
+ * print_number - affiche un entier avec _putchar
+ * @n: entier à afficher
+ */
 void print_number(int n)
 {
-if (n < 0)
-{
-_putchar('-');
-if (n / 10 == 0)
-{
-_putchar('0' - (n % 10));
-return;
-}
-n = -n;
-}
-if (n < 10)
-{
-_putchar(n + '0');
-}
+    /* Affiche le signe une seule fois si n est négatif */
+    if (n < 0)
+    {
+        _putchar('-');
+        if (n / 10 == 0)
+        {
+            _putchar('0' - (n % 10));
+            return;
+        }
+        n = -n; /* On rend n positif pour continuer la récursion sans réafficher '-' */
+    }
 
-else
-{
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
-print_number(n / 10);
-_putchar((n % 10) + '0');
-}
+    if (n / 10)
+        print_number(n / 10);
+
+    _putchar((n % 10) + '0');
 }
