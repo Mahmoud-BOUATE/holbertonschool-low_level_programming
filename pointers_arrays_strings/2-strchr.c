f95febaafd09f79b7600ac58f74a69c4c4227e78
mahmoud BOUATE <12065@holbertonstudents.com>
2025-11-03 13:33:45 +0000

    test

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr - function that locates a character in a string.
+ * @s:  Pointeur
+ * @c: character locates
+ * Return: character locates
+ */
+char *_strchr(char *s, char c)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] == c)
+return (s + i);
+i++;
+}
+return (NULL);
+}
