2c3fd61559548171fe08a5b7237e779279bcc274
mahomud BOUATE <12065@holbertonstudent.com>
2025-11-04 12:45:26 +0000

    test

---
+++ recursion/3-factorial.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* factorial - function  that returns the length of a string.
+* @n: pointeur
+*
+ * Return: factorial
+*/
+int factorial(int n)
+{
+if (n == 1)
+{
+return (1);
+}
+else if (n < 0)
+{
+return (-1);
+}
+return n*factorial (n - 1);
+}
