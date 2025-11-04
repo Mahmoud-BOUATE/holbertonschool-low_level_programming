e0cb18599dc205b1852cd524efcdcf96905d578c
mahomud BOUATE <12065@holbertonstudent.com>
2025-11-04 13:32:41 +0000

    test

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* factorial - function  that returns the length of a string.
+* @n: pointeur
+*
+ * Return: factorial
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+{
+return (1);
+}
+return x * _pow_recursion(x, y - 1);
+}
+
