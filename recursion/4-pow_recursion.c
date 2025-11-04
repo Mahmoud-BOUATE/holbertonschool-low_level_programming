93b3a0d9f14413c66d3c5fdc104e90dc30a5598e
mahomud BOUATE <12065@holbertonstudent.com>
2025-11-04 14:29:37 +0000

    g

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,22 +1,24 @@
 #include "main.h"
 
+/* helper récursif privé : teste les candidats x = 0,1,2,... */
+static int _sqrt_check(int n, int x)
+{
+long sq = (long)x * x;
+if (sq == n)
+return (x);
+if (sq > n)
+return (-1);
+return (_sqrt_check(n, x + 1));
+}
 /**
  * _sqrt_recursion - returns the natural square root of n
- * @n: the number
- * @x: current candidate for the square root (start with 1)
- * Return: the natural square root, or -1 if it doesn't exist
+ * @n: number to check
+ *
+ * Return: natural square root, or -1 if none exists
  */
-int _sqrt_recursion_helper(int n, int x)
-{
-if (x * x == n)
-return x;
-if (x * x > n)
-return -1;
-return _sqrt_recursion_helper(n, x + 1);
-}
 int _sqrt_recursion(int n)
 {
 if (n < 0)
-return -1;
-return _sqrt_recursion_helper(n, 1);
+return (-1);
+return (_sqrt_check(n, 0));
 }
