35d5949af597538fdc83193371dbc673b312c5f7
mahmoud BOUATE <12065@holbertonstudents.com>
2025-11-04 11:24:54 +0000

    test

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
 * _print_rev_recursion - function that prints a string, followed by a new line.
 * @s: pointeur
@@ -8,7 +7,6 @@ void _print_rev_recursion(char *s)
 {
 if (*s == '\0')
 {
-_putchar('\n');
 return;
 }
 _print_rev_recursion (s + 1);
