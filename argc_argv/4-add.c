da780b95dff4e6f580c68bd9341f921779552f9b
mahmoud BOUATE <12065@holbertonstudent.com>
2025-11-05 11:20:49 +0000

    u

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -6,7 +6,6 @@
  * main - Adds positive numbers passed as command line arguments
  * @argc: Number of arguments
  * @argv: Array of arguments
- * 
  * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
@@ -16,7 +15,7 @@ int i, j;
 if (argc == 1) /* No numbers provided */
 {
 printf("0\n");
-return 0;
+return (0);
 }
 for (i = 1; i < argc; i++)
 {
@@ -25,11 +24,11 @@ for (j = 0; argv[i][j] != '\0'; j++)
 if (!isdigit(argv[i][j]))
 {
 printf("Error\n");
-return 1;
+return (1);
 }
 }
 sum += atoi(argv[i]);
 }
 printf("%d\n", sum);
-return 0;
+return (0);
 }
