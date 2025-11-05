57a738956972da04f186bb088920f3af51cf389d
mahmoud BOUATE <12065@holbertonstudent.com>
2025-11-05 09:38:50 +0000

    test

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints the program name followed by a new line
+ * @argc: number of arguments (unused)
+ * @argv: array of argument strings
+ *
+ * Return: 0 on success
+ */
+int main(int argc , char **argv __attribute__((unused)))
+{
+printf("%d\n", argc);
+return (0);
+}
+
+
