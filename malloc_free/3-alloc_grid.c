2856520aaf993e1322100bb5880117d2f28e31d2
mahmoud BOUATE <12065@holbertonstudents.com>
2025-11-10 21:46:20 +0000

    h

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -15,17 +15,19 @@ int i, j;
 if (width <= 0 || height <= 0)
 return (NULL);
 grid = malloc(sizeof(int *) * height);
-if (grid[i] == NULL)
+if (grid == NULL)
 return (NULL);
+
+for (i = 0; i < height; i++)
+{
+grid[i] = malloc(sizeof(int) * width);
+if (grid[i] == NULL)
 {
 for (j = 0; j < i; j++)
 free(grid[j]);
 free(grid);
 return (NULL);
 }
-for (i = 0; i < height; i++)
-{
-grid[i] = malloc(sizeof(int) * width);
 for (j = 0; j < width; j++)
 grid[i][j] = 0;
 }
