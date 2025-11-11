1a29b638d968ba90b424c3dc5afe93552d1cea59
mahmoud BOUATE <12065@holbertonstudents.com>
2025-11-11 17:52:43 +0000

    calloc function

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_calloc -  function that allocates memory for an array, using malloc.
+ * @nmemb: nombre d'élément du tableau
+ * @size: la taille en octets
+ * Return: pointeur vers la zone mémoire allouée
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int *p1;
+unsigned int i;
+p1 = 0;
+p1 = malloc(size * nmemb);
+if (p1 == NULL)
+return (NULL);
+if (nmemb == 0 || size == 0)
+return (NULL);
+for (i = 0; i < p1[i]; i++)
+{
+p1[i] = 0;
+}
+return (p1);
+}
--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -4,4 +4,5 @@
 void *malloc_checked(unsigned int b);
 int main(void);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 #endif
