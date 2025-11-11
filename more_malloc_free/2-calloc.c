f7e1f926bc88aa4fdd22104091cdc24a1f115b3a
mahmoud BOUATE <12065@holbertonstudents.com>
2025-11-11 18:08:46 +0000

    teste

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -14,12 +14,12 @@ unsigned int i;
 if (nmemb == 0 || size == 0)
 return (NULL);
 p1 = malloc(size * nmemb);
+if (p1 == NULL)
+return (NULL);
 i = 0;
 for (i = 0; i < nmemb; i++)
 {
 p1[i] = 0;
 }
-if (p1 == NULL)
-return (NULL);
 return (p1);
 }
