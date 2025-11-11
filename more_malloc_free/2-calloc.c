a8b0402fb9e26370508859d9ebb7aff339cdf16e
mahmoud BOUATE <12065@holbertonstudents.com>
2025-11-11 19:16:32 +0000

    calloc function

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,7 +10,6 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 int *p1;
-unsigned int i;
 size_t total;
 
 if (nmemb == 0 || size == 0)
@@ -21,10 +20,7 @@ total = (size_t)nmemb * (size_t)size;
 p1 = malloc(total);
 if (p1 == NULL)
 return (NULL);
-i = 0;
-for (i = 0; i < nmemb; i++)
-{
-p1[i] = 0;
-}
+
+ memset(p1, 0, total);
 return (p1);
 }
