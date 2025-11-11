78eda01e53ecec1d932acb76145337da0f7c8262
mahmoud BOUATE <12065@holbertonstudents.com>
2025-11-11 17:56:31 +0000

    calloc fucntion

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,14 +12,15 @@ unsigned int *p1;
 unsigned int i;
 p1 = 0;
 p1 = malloc(size * nmemb);
-if (p1 == NULL)
-return (NULL);
-if (nmemb == 0 || size == 0)
-return (NULL);
 i = 0;
 for (i = 0; i < p1[i]; i++)
 {
 p1[i] = 0;
 }
+if (p1 == NULL)
+return (NULL);
+if (nmemb == 0 || size == 0)
+return (NULL);
+
 return (p1);
 }
