39deba465278432f0b08803d0a86896431ebedad
mahmoud BOUATE <12065@holbertonstudents.com>
2025-11-12 14:41:02 +0000

    ye

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -6,13 +6,10 @@
  */
 void print_dog(struct dog *d)
 {
-if (d == NULL)
-return;
 if (d->name == NULL || d->owner == NULL)
 printf("(nil)");
-if (d-> age == 0)
-printf("(nil)");
-
+if (d == NULL)
+return;
 printf("Name: %s\n", d->name);
 printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner);
