c402e02b0b1d8a7275267552f5c31dd8960f5eab
mahmoud BOUATE <12065@holbertonstudents.com>
2025-11-12 10:57:05 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,17 @@
-#include <stdio.h>
-
-struct dog {
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure qui définit un chien
+ * @name: nom du chien (chaîne de caractères)
+ * @age: âge du chien (nombre flottant)
+ * @owner: nom du propriétaire (chaîne de caractères)
+ *
+ * Description: structure représentant les informations principales d’un chien.
+ */
+struct dog
+{
 char *name;
 float age;
 char *owner;
 };
+#endif /* DOG_H */
