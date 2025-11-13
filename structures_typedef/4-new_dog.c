1f7325eefee2f14fc75ac46c6bd30d7a4dcdce6e
mahmoud BOUATE <12065@holbertonstudents.com>
2025-11-13 10:31:58 +0000

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,48 +3,51 @@
 #include <string.h>
 #include <stdlib.h>
 /**
- * *new_dog - creer un nouveau chien
- * @name: non du chien
- * @owner: prop du chien
- * @age: age du pointeur
- *
-*Return: retourne d
+ * new_dog - créer un nouveau chien
+ * @name: nom du chien
+ * @owner: propriétaire du chien
+ * @age: âge du chien
+ * Return: pointeur vers la nouvelle structure dog_t, ou NULL si échec
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *d;
-char *copy_name;
-char *copy_owner;
-int i , j;
-
+char *copy_name, *copy_owner;
+int i, j;
 d = malloc(sizeof(dog_t));
 if (d == NULL)
 return (NULL);
-
-
+if (name != NULL)
+{
 copy_name = malloc(strlen(name) + 1);
 if (copy_name == NULL)
+{
+free(d);
 return (NULL);
-
+}
 for (i = 0; name[i]; i++)
 copy_name[i] = name[i];
-
 copy_name[i] = '\0';
 d->name = copy_name;
-
+}
+else
+d->name = NULL;
+if (owner != NULL)
+{
 copy_owner = malloc(strlen(owner) + 1);
 if (copy_owner == NULL)
-return (NULL);
-
-j = 0;
-while (owner[j] != '\0')
 {
-copy_owner[j] = owner[j];
-j++;
+free(d->name);
+free(d);
+return (NULL);
 }
+for  (j = 0 ; owner[j]; j++)
+copy_owner[j] = owner[j];
 copy_owner[j] = '\0';
 d->owner = copy_owner;
-
+}
+else
+d->owner = NULL;
 d->age = age;
 return (d);
 }
