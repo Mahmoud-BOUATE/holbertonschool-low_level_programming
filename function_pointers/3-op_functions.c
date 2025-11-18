b3472a1a4d04226a09406ebb59d4167efdbd4cc2
mahmoud BOUATE <12065@holbertonschool.com>
2025-11-18 08:38:43 +0000

    add operator function

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,36 @@
+#include "CALC.H"
+
+/**
+* op_add - function that add 2 integers
+* Return: return the addition of 2 integers
+*/
+int op_add(int a, int b)
+return (a + b);
+
+/**
+* op_sub - function that sub 2 integers
+* Return: return the sub of 2 integers
+*/
+int op_sub(int a, int b)
+return (a - b);
+
+/**
+* op_mul - function that mul 2 integers
+* Return: return the mul of 2 integers
+*/
+int op_mul(int a, int b)
+return (a * b);
+
+/**
+* op_div - function that div 2 integers
+* Return: return the div of 2 integers
+*/
+int op_div(int a, int b);
+return (a / b);
+
+/**
+* op_mod - function that mod 2 integers
+* Return: return the modulo of 2 integers
+*/
+int op_mod(int a, int b);
+return (a / b);
