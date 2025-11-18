d3b251890897149554f70608105cfb5663786f7f
mahmoud BOUATE <12065@holbertonschool.com>
2025-11-18 13:22:39 +0000

    test

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,5 +1,5 @@
-#ifndef CALC.H
-#define CALC.H
+#ifndef CALC_H
+#define CALC_H
 /**
  * struct op - Struct op
  *
@@ -18,6 +18,9 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 
+int main(int argc, char *argv[]);
+int (*get_op_func(char *s))(int, int);
 
-#endif /* CALC.H */
+
+#endif /* CALC_H */
 
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -1,36 +1,52 @@
-#include "CALC.H"
+#include "3-calc.h"
+#include <stdio.h>
 
 /**
 * op_add - function that add 2 integers
 * Return: return the addition of 2 integers
 */
 int op_add(int a, int b)
-return (a + b);
+{
+return(a + b);
+}
+
 
 /**
 * op_sub - function that sub 2 integers
 * Return: return the sub of 2 integers
 */
 int op_sub(int a, int b)
-return (a - b);
+{
+return(a - b);
+}
+
 
 /**
 * op_mul - function that mul 2 integers
 * Return: return the mul of 2 integers
 */
 int op_mul(int a, int b)
-return (a * b);
+{
+return(a * b);
+}
+
 
 /**
 * op_div - function that div 2 integers
 * Return: return the div of 2 integers
 */
-int op_div(int a, int b);
-return (a / b);
+int op_div(int a, int b)
+{
+return(a / b);
+}
+
 
 /**
 * op_mod - function that mod 2 integers
 * Return: return the modulo of 2 integers
 */
-int op_mod(int a, int b);
-return (a / b);
+int op_mod(int a, int b)
+{
+return (a % b);
+}
+
