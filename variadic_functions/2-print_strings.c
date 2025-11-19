7ddd96c00a54a9776cb19f29172f03edd676993c
mahmoud BOUATE <12065@holbertonschool.com>
2025-11-19 15:03:14 +0000

    add print strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+* print_strings - function that prints strings
+* @n: le nombre d'arguements
+* @separator: is the string to be printed between numbers
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+char *s;
+va_list ap;
+va_start(ap, n);
+
+for (i = 0; i < n; i++)
+{
+s = va_arg(ap, char *);
+printf("%s", s);
+
+if (separator != NULL && i < n - 1)
+printf("%s", separator);
+}
+printf("\n");
+va_end(ap);
+}
