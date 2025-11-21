c9cbd5a38f67b2f7804aad51e7c024a543e466e1
mahmoud BOUATE <12065@holbertonschool.com>
2025-11-21 16:58:17 +0000

    printf all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,37 +1,56 @@
 #include "variadic_functions.h"
 #include <stdio.h>
+#include <stdarg.h>
 
-typedef struct format
-{
-char s;
-int (*print)(char)
-}fmt;
 
-void printf_char(va_list(*args))
-{
-printf("%c",char)    
-}
 
 void print_all(const char * const format, ...)
 {
-    op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};   
-}
+int i = 0, e;
+char c;
+char *s;
+const char *sep = "";
+va_list ap;
 
 
+va_start(ap, format);
 
-
-
-
-
-
-int main (void)
+while (format[i] != '\0')
 {
 
+switch (format[i])
+{
+case 'c':
+c = va_arg(ap, int);
+printf("%s",sep);
+printf("%c", c);
+sep = ", ";
+break;
+
+case 'i':
+e = va_arg(ap, int);
+printf("%s",sep);
+printf("%d", e);
+sep = ", ";
+break;
+
+case 's':
+s = va_arg(ap, char *);
+printf("%s",sep);
+
+printf("%s", s);
+sep = ", ";
+break;
+
+default:
+break;
 }
+i++;
+}
+
+va_end(ap);
+printf("\n");
+}
+
+
+
