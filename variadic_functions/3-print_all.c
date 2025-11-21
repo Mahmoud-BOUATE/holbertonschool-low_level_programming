743d1aee3958bce06c4fbb859b5e9200863f95bb
mahmoud BOUATE <12065@holbertonschool.com>
2025-11-21 17:05:13 +0000

    printf all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,56 +1,48 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-
-
-
+/**
+* print_all - function that prints anything.
+* @format: le nombre d'arguements
+*/
 void print_all(const char * const format, ...)
 {
 int i = 0, e;
 char c;
 char *s;
 const char *sep = "";
+float f;
 va_list ap;
-
-
 va_start(ap, format);
-
 while (format[i] != '\0')
 {
-
 switch (format[i])
 {
 case 'c':
 c = va_arg(ap, int);
-printf("%s",sep);
-printf("%c", c);
+printf("%s %c", sep, c);
 sep = ", ";
 break;
-
 case 'i':
 e = va_arg(ap, int);
-printf("%s",sep);
-printf("%d", e);
+printf("%s %d", sep, e);
 sep = ", ";
 break;
-
+case 'f':
+f = va_arg(ap, double);
+printf("%s %f", sep, f);
+sep = ", ";
+break;
 case 's':
 s = va_arg(ap, char *);
-printf("%s",sep);
-
-printf("%s", s);
+printf("%s %s", sep, s);
 sep = ", ";
 break;
-
 default:
 break;
 }
 i++;
 }
-
 va_end(ap);
 printf("\n");
 }
-
-
-
