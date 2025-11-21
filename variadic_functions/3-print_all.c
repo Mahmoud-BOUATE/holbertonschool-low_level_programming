fa4afb3d9a4cc409afaa1502a146472461bb30e8
mahmoud BOUATE <12065@holbertonschool.com>
2025-11-21 16:01:34 +0000

    print all

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+typedef struct format
+{
+char s;
+int (*print)(char)
+}fmt;
+
+void printf_char(va_list(*args))
+{
+printf("%c",char)    
+}
+
+void print_all(const char * const format, ...)
+{
+    op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};   
+}
+
+
+
+
+
+
+
+
+int main (void)
+{
+
+}
