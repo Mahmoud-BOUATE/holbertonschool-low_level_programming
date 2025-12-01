68da6ad3855137af3be446fdb3fbb3d55f0db2c1
mahmoud BOUATE <12065@holbertonschool.com>
2025-12-01 21:37:43 +0100

    Fix add_node function to correctly set the length of the new node's string

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -16,6 +16,7 @@ new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
 return (NULL);
 new_node->str = strdup(str);
+new_node->len = strlen(str);
 if (new_node->str == NULL)
 {
 free(new_node);
