d0f20e89c4eef80bfa173271cff8bc5e542349b7
mahmoud BOUATE <12065@holbertonschool.com>
2025-12-01 21:41:40 +0100

    Fix add_node function to correctly calculate the length of the new node's string

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,6 +11,7 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+unsigned int len = 0;
 list_t *new_node;
 new_node = malloc(sizeof(list_t));
 if (new_node == NULL)
@@ -21,7 +22,9 @@ if (new_node->str == NULL)
 free(new_node);
 return (NULL);
 }
-new_node->len = strlen(str);
+while (str[len] != '\0')
+len++;
+new_node->len = len;
 new_node->next = *head;
 *head = new_node;
 return (new_node);
