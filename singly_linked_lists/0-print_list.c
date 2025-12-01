eaf5dee30509f120ed998596e451e0a6ecf31f71
mahmoud BOUATE <12065@holbertonschool.com>
2025-12-01 19:43:38 +0100

    Fix formatting in print_list function for consistency

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,10 +12,9 @@ size_t print_list(const list_t *h)
 {
 size_t count;
 count = 0;
-while (h != NULL)
-{
-if (h->str == NULL)
-printf("[0] (nil)\n");
+while (h != NULL){
+    if(h->str == NULL)
+    printf("[0] (nil)\n");
 else
 printf("[%u] %s\n", h->len, h->str);
 count++;
