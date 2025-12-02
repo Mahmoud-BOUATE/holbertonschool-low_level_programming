cd0bf2395ed24cc51567df1bfdcb2b87d5ab3ffb
mahmoud BOUATE <12065@holbertonschool.com>
2025-12-02 19:22:18 +0100

    Implement doubly linked list structure and functions for node manipulation

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * print_dlistint - function that add printf all the chaine
+ * @h: pointer to first node
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+size_t count;
+count = 0;
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+count++;
+}
+
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->prev;
+count++;
+}
+return (count);
+}
