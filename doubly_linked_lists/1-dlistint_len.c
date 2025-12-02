63edf71eaf8f159e31d09ea65b5562b391e09092
mahmoud BOUATE <12065@holbertonschool.com>
2025-12-02 19:30:28 +0100

    Add dlistint_len function to count elements in a doubly linked list

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * dlistint_len - function that returns the number of elements of the chain
+ * @h: pointer to first node
+ * Return: number of elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t len;
+while (h != NULL)
+{
+h = h->next;
+len++;
+}
+return (len);
+}
