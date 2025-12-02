9f695b560a75114ee846fca7e667830a1cea4988
mahmoud BOUATE <12065@holbertonschool.com>
2025-12-02 21:48:11 +0100

    Add free_dlistint function to free a doubly linked list

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * *add_dnodeint_end - function that free
+ * @head: pointer to pointeur to first node
+ */
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *tmp;
+
+while (head != NULL)
+{
+tmp = head->next;
+free(head);
+head = tmp;
+}
+}
