52294d6611f2a6bd161be3692c025e2922ad6216
mahmoud BOUATE <12065@holbertonschool.com>
2025-12-03 22:57:08 +0100

    Remove _putchar function declaration from main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 size_t read_textfile(const char *filename, size_t letters);
-int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 #endif /* MAIN_H */
 
