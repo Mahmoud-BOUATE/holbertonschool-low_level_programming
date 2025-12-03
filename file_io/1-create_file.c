5e323dfa1af93a88e34d89986efed477f3e75527
mahmoud BOUATE <12065@holbertonschool.com>
2025-12-03 22:36:29 +0100

    Add create_file and _putchar functions; update main to test file creation

---
+++ file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int create_file(const char *filename, char *text_content)
+{
+int fd, count, nb_ecrits;
+char *ptr;
+if (filename == NULL)
+return (-1);
+count = 0;
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+if (fd == -1)
+return (-1);
+
+if (text_content != NULL)
+{
+ptr = text_content;
+while (*ptr != '\0')
+{
+count++;
+ptr++;
+}
+nb_ecrits = write(fd, text_content, count);
+if (nb_ecrits != count)
+{
+close(fd);
+return (-1);
+}
+}
+if (close(fd) == -1)
+return (-1);
+return (1);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -2,5 +2,7 @@
 #define MAIN_H
 int main(int ac, char **av);
 ssize_t read_textfile(const char *filename, size_t letters);
+int _putchar(char c);
+int create_file(const char *filename, char *text_content);
 #endif /* MAIN_H */
 
