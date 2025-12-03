92b87abe3bd065a3eaa3d730994adafdd0ce51e1
mahmoud BOUATE <12065@holbertonschool.com>
2025-12-03 22:40:25 +0100

    Remove main function declaration from main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-int main(int ac, char **av);
 size_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 int create_file(const char *filename, char *text_content);
