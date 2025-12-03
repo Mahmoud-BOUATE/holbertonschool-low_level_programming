7bba6c787382f441d024b115e0e32601a8954491
mahmoud BOUATE <12065@holbertonschool.com>
2025-12-03 22:55:58 +0100

    Add append_text_to_file function and main test file

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -3,23 +3,22 @@
 #include <unistd.h>
 #include <fcntl.h>
 /**
- * create_file - une focntion pour créer un ficher
+ * append_text_to_file -ajouter à du contenu à la fin d'un fichier
  * @filename: nom du fichier à créer
  * @text_content: contenue à écrire
  *
  * Return: nombre réel de lettres lues et affichées, ou 0 en cas d'erreur
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 int fd, count, nb_ecrits;
+count = 0;
 char *ptr;
 if (filename == NULL)
 return (-1);
-count = 0;
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
-
 if (text_content != NULL)
 {
 ptr = text_content;
@@ -28,6 +27,7 @@ while (*ptr != '\0')
 count++;
 ptr++;
 }
+
 nb_ecrits = write(fd, text_content, count);
 if (nb_ecrits != count)
 {
@@ -39,4 +39,38 @@ if (close(fd) == -1)
 return (-1);
 
 return (1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return(1);
 }
