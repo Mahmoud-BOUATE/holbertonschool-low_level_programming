fd60d0582f9161c17b68fa03fe9ab9f89a5cc181
mahmoud BOUATE <12065@holbertonschool.com>
2025-12-04 20:56:57 +0100

    test

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -12,7 +12,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 unsigned long int hash;
 
 if (!key || size == 0)
-return 0;
+return (0);
 hash = hash_djb2(key);
 return (hash % size);
 }
